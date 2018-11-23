a83d618499e150a2fed2e7a4048e14f8a4a0cc3d
jberry93 <jeffreyberry93@gmail.com>
2018-11-23 13:04:33 -0800

    feat(timeConversion): back at it again! Finished time conversion challenge

--- Notes/HackerRank/Algorithms/Warmup.js
+++ Notes/HackerRank/Algorithms/Warmup.js
@@ -6,22 +6,22 @@
 
 // Simple Array Sum
 function simpleArraySum(n, ar) {
-  let sum = 0;
-  for (let i = 0; i < n; i++) {
-    sum += ar[i] ? ar[i] : 0;
-  }
-  return sum;
+    let sum = 0;
+    for (let i = 0; i < n; i++) {
+        sum += ar[i] ? ar[i] : 0;
+    }
+    return sum;
 }
 
 // Compare the Triplets
 function solve(a0, a1, a2, b0, b1, b2) {
-  let A = 0;
-  let B = 0;
-  // I love the terseness of the ternary operator :)
-  a0 > b0 ? (a0 !== b0 ? A++: null) : (a0 !== b0 ? B++ : null);
-  a1 > b1 ? (a1 !== b1 ? A++: null) : (a1 !== b1 ? B++ : null);
-  a2 > b2 ? (a2 !== b2 ? A++: null) : (a2 !== b2 ? B++ : null);
-  return [A, B];
+    let A = 0;
+    let B = 0;
+    // I love the terseness of the ternary operator :)
+    a0 > b0 ? (a0 !== b0 ? A++: null) : (a0 !== b0 ? B++ : null);
+    a1 > b1 ? (a1 !== b1 ? A++: null) : (a1 !== b1 ? B++ : null);
+    a2 > b2 ? (a2 !== b2 ? A++: null) : (a2 !== b2 ? B++ : null);
+    return [A, B];
 }
 
 // A Very Big Sum
@@ -34,31 +34,31 @@ function solve(a0, a1, a2, b0, b1, b2) {
 
 // Diagonal Difference
 function solve(n, a) {
-  let x = 0;
-  let y = 0;
-  let xcounter = 0;
-  let ycounter = n - 1;
-  for (let arr of a) {
-    x += arr[xcounter];
-    y += arr[ycounter];
-    xcounter++;
-    ycounter--;
-  }
-  let ans = x > y ? x - y : y - x;
-  console.log(ans);
+    let x = 0;
+    let y = 0;
+    let xcounter = 0;
+    let ycounter = n - 1;
+    for (let arr of a) {
+        x += arr[xcounter];
+        y += arr[ycounter];
+        xcounter++;
+        ycounter--;
+    }
+    let ans = x > y ? x - y : y - x;
+    console.log(ans);
 }
 
 // Plus Minus
 function solve(n, arr) {
-  let pos = 0;
-  let neg = 0;
-  let zero = 0;
-  for (let i of arr) {
-    i === 0 ? zero++ : (i > 0 ? pos++ : (i < 0 ? neg++ : null));
-  }
-  console.log((pos / n).toPrecision(6));
-  console.log((neg / n).toPrecision(6));
-  console.log((zero / n).toPrecision(6));
+    let pos = 0;
+    let neg = 0;
+    let zero = 0;
+    for (let i of arr) {
+        i === 0 ? zero++ : (i > 0 ? pos++ : (i < 0 ? neg++ : null));
+    }
+    console.log((pos / n).toPrecision(6));
+    console.log((neg / n).toPrecision(6));
+    console.log((zero / n).toPrecision(6));
 }
 
 
@@ -81,24 +81,24 @@ function solve(n) {
 }
 
 // Min-Max Sum
-function main() new Promise(function(resolve, reject) {
-  arr = readLine().split(' '); // provided
-  arr = arr.map(Number); // provided
+function main() {
+    arr = readLine().split(' '); // provided
+    arr = arr.map(Number); // provided
 
-  arr.sort();
-  let min = 0;
-  let max = 0;
+    arr.sort();
+    let min = 0;
+    let max = 0;
 
-  for (let i = 0; i < 4; i++) {
-    min += arr[i];
-  }
+    for (let i = 0; i < 4; i++) {
+        min += arr[i];
+    }
 
-  for (let j = arr.length - 1; j > 0; j--) {
-    max += arr[j];
-  }
+    for (let j = arr.length - 1; j > 0; j--) {
+        max += arr[j];
+    }
 
-  console.log(min + ' ' + max);
-});
+    console.log(min + ' ' + max);
+};
 
 // Birthday Cake Candles
 /**
@@ -136,4 +136,29 @@ function birthdayCakeCandles(n, ar) {
         answer = table[key];
     }
     return answer;
+}
+
+// Time Conversion
+function timeConversion(s) {
+    const timeSplit = s.split(':');
+    const AMorPM = timeSplit[2].slice(2);
+    let hour = timeSplit[0];
+    let minutes = timeSplit[1];
+    let seconds = timeSplit[2].split('')[0] + timeSplit[2].split('')[1];
+
+    if (hour === '12' && AMorPM === 'AM') {
+        return `00:${minutes}:${seconds}`;
+    }
+
+    if (hour === '12' && AMorPM === 'PM') {
+        return `12:${minutes}:${seconds}`;
+    }
+
+    if (AMorPM === 'PM') {
+        return `${12 + Number(hour)}:${minutes}:${seconds}`;
+    }
+
+    if (AMorPM === 'AM') {
+        return `${hour}:${minutes}:${seconds}`;
+    }
 }
