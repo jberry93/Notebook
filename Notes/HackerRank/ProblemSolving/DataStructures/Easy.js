933a444cc4c5076399b2d3f38eacb98bffea67ad
jberry93 <jeffreyberry93@gmail.com>
2018-12-01 22:29:28 -0800

    feat(dataStrucs): start data structures section and move around folders

---
+++ Notes/HackerRank/ProblemSolving/DataStructures/Easy.js
@@ -0,0 +1,9 @@
+/**
+ * Answers to all "Easy" challenges in Data Structures section.
+ * There may be better answers but these ones got 100%!
+ */
+
+// Array - DS
+function reverseArray(a) {
+    return a.reverse();
+}
