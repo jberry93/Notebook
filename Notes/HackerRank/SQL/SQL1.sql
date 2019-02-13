974b71964cd6d489d3691d78fc10e7a80d498a21
jberry93 <jeffreyberry93@gmail.com>
2019-02-12 20:47:34 -0800

    feat(selectQuery2): completed Revising the Select Query 2 challenge

--- Notes/HackerRank/SQL/SQL1.sql
+++ Notes/HackerRank/SQL/SQL1.sql
@@ -1,10 +1,23 @@
 /*
 Revising the Select Query 1
-- Query all columnds for all American
+
+Query all columnds for all American
 cities in CITY with populations larger
 than 100000. The CountryCode for America
 is USA
 */
 SELECT Id, Name, CountryCode, District, Population FROM City
 WHERE Population > 100000
+AND CountryCode = 'USA';
+
+/*
+Revising the Select Query 2
+
+Query the names of all American cities
+in CITY with populations larger than
+120000. The CountryCode for America is
+USA
+*/
+SELECT Name FROM City
+WHERE Population > 120000
 AND CountryCode = 'USA';
